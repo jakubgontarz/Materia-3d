29904dc0df20b2fd06257c6d9aa8de8bec0614f3
jakubgontarz <126570441+jakubgontarz@users.noreply.github.com>
2026-08-25 11:40:38 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: 'Materia-3d',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
